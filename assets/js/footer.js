e5d49e36bdff9f43fb0e030e53fde9b6127477f1
Jorge Mejicanos <jorgemejicanos145@gmail.com>
2023-11-28 16:24:07 -0600

    Politicas, comunicados y Galeria

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 function loadFooter() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'footer.html', true);
+    xhr.open('GET', '/footer.html', true);
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         // Insertar el contenido del pie de página en el contenedor específico
--- assets/js/menus.js
+++ assets/js/menus.js
@@ -1,7 +1,7 @@
 // Definir una función para cargar la barra de navegación
 function loadNavbar() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'navbar.html', true);
+    xhr.open('GET', '/navbar.html', true);
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         // Insertar el contenido de la barra de navegación en un elemento con un ID específico
